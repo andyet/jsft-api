493ada8ee2f54ba1d73e535e0c41630f5cf56b7c
Philip Roberts <phil@andyet.net>
2014-07-17 23:37:11 -0700

    Fix tweets without mentions being broken

--- find-mentions.js
+++ find-mentions.js
@@ -3,6 +3,9 @@ var User = require('./models/user');
 
 module.exports = function (content, done) {
     var usernames = content.match(/@\w+/g);
+
+    if (!usernames) return done(null, []);
+
     usernames = usernames.map(function (username) {
         return username.substr(1);
     });
