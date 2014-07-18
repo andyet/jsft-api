b5d4ce88d343ffd1a1d2e03b26d0b349a0c1f9ee
Philip Roberts <phil@andyet.net>
2014-07-17 23:41:04 -0700

    Fix unfound mentions

--- find-mentions.js
+++ find-mentions.js
@@ -7,13 +7,18 @@ module.exports = function (content, done) {
     if (!usernames) return done(null, []);
 
     usernames = usernames.map(function (username) {
-        return username.substr(1);
+        return username.substr(1).toLowerCase();
     });
 
     async.map(usernames, function (username, next) {
-        User.findByIndex('username', username, next);
+        User.findByIndex('username', username, function (err, user) {
+            if (err) user = null;
+
+            return next(null, user);
+        });
     }, function (err, users) {
         if (err) return done(err);
+        users = users.filter(function (user) { return !!user; });
 
         done(null, users.map(function (u) { return u.id; }));
     });
